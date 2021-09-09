f24981b9ad0eed31c90d1572f0ea3f19bddd8522
seifediine <seifeddine.elkorichi@hotmail.com>
2021-09-09 11:41:44 +0100

    verify token and get user credentials

--- Routes/api/auth.js
+++ Routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 
 // @route:      GET /api/auth
-// Description: Test Route
+// Description: Auth Route
 // Access:      Public
-router.get('/', (req, res) => res.send('Auth Route'))
+router.get('/', auth, async (req, res) => {
+  // Make a call to the database to get the user
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 
 module.exports = router
