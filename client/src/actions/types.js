26883cfa2f6a60f98e66c909974e99117be67138
seifediine <seifeddine.elkorichi@hotmail.com>
2021-09-26 20:48:36 +0100

    load user and set authentication token

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,3 +5,7 @@ export const REMOVE_ALERT = 'REMOVE_ALERT'
 // Register Constants
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 export const REGISTER_FAIL = 'REGISTER_FAIL'
+
+// Load a User & Set Auth Token
+export const USER_LOADED = 'USER_LOADED'
+export const AUTH_ERROR = 'AUTH_ERROR'
